1d2828d4882f07fb3fbabcab73e09fe1732249af
ceed <bg201801625@wmsu.edu.ph>
2021-02-09 16:25:36 +0800

    staff with datatable

--- src/Pages/Staff.js
+++ src/Pages/Staff.js
@@ -1,7 +1,80 @@
+/*https://material-table.com/#/docs/get-started - LINK FOR MATERIAL-TABLE*/ 
+import MaterialTable from 'material-table';
+import React from 'react';
+
 const Staff = () => {
-    return ( 
-        <div>Staff</div>
-     );
+
+    const { useState } = React;
+
+  const [columns, setColumns] = useState([
+    {
+      title: 'Name', field: 'name',
+      editComponent: props => (
+        <input
+          type="text"
+          value={props.value}
+          onChange={e => props.onChange(e.target.value)}
+        />
+      )
+    },
+    { title: 'Surname', field: 'surname' },
+    { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+    {
+      title: 'Birth Place',
+      field: 'birthCity',
+      lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+    },
+  ]);
+
+  const [data, setData] = useState([
+    { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+    { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+  ]);
+
+  return (
+    <MaterialTable
+      title="Users"
+      columns={columns}
+      data={data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+              
+              resolve();
+            }, 1000)
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
+
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+
+              resolve();
+            }, 1000)
+          }),
+      }}
+      options={{
+          filtering: true,
+          grouping: true
+      }}
+    
+    />
+  )
 }
  
 export default Staff;
