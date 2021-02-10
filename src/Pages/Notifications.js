49e9d0462a0090e4237b474a441ea26f7f91e24d
ceed <bg201801625@wmsu.edu.ph>
2021-02-10 16:43:24 +0800

    Notif - card styling

--- src/Pages/Notifications.js
+++ src/Pages/Notifications.js
@@ -1,7 +1,51 @@
-const Notification = () => {
-    return ( 
-        <div>Notifications</div>
-     );
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+
+});
+
+export default function Notifications() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root} 
+        style= {{
+           margin: "1.5rem",
+           borderRadius: "12px",
+           boxShadow: "0px 20px 16.83px 0.17px rgba(0, 0, 0, 0.05)",
+        }}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+         Hello
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
- 
-export default Notification;
